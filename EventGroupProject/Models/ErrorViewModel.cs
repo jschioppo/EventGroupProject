5dcf0e7a7be83f860ce890f0b4bdb1f5003ed2ee
Jacob Schioppo <jacobschioppo@gmail.com>
2018-08-30 23:48:20 -0400

    Add project files.

---
+++ EventGroupProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EventGroupProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
