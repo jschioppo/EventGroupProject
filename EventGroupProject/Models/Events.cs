5ccca892068bbac999abfa132c2db3f2ae767c3c
mmonnik <mmonnik@mail.usf.edu>
2018-09-20 10:48:20 -0400

    Event model

---
+++ EventGroupProject/Models/Events.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventGroupProject.Models
+{
+    public class Events
+    {
+        public char City { get; set; }
+        public char Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; }
+        public char Location { get; set; }
+        public int Price { get; set; }
+    }
+}
