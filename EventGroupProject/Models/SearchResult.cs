f07f4734758c2d14efd592a6364d9b4e994d0db9
Jacob Schioppo <jacobschioppo@gmail.com>
2018-09-25 19:54:41 -0400

    Added search result model

---
+++ EventGroupProject/Models/SearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventGroupProject.Models
+{
+    public class SearchResult
+    {
+        public string City { get; set; }
+        public List<Tag> Tags { get; set; }
+        //TODO: Needs to be uncommented after Event model is added
+        //public List<Event> ResultEvents { get; set; }
+
+    }
+}
