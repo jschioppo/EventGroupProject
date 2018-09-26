4aa18452134bb749ff7351e006b4d9a3557c29e3
jschioppo <jacobschioppo@gmail.com>
2018-09-25 22:19:06 -0400

    Update Events.cs

--- EventGroupProject/Models/Events.cs
+++ EventGroupProject/Models/Events.cs
@@ -7,11 +7,12 @@ namespace EventGroupProject.Models
 {
     public class Events
     {
-        public char City { get; set; }
-        public char Description { get; set; }
+        public int EventId { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
         public DateTime StartTime { get; set; }
         public int Duration { get; set; }
-        public char Location { get; set; }
+        public string Location { get; set; }
         public int Price { get; set; }
     }
 }
