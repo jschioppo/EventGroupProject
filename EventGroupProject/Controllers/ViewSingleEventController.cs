d2774b55d3a09f50dd8a8a501bd319a4e5c4656c
Jacob Schioppo <jacobschioppo@gmail.com>
2018-10-15 02:47:25 -0400

    Added new model for comments that contains extra info Added method to send comments to view Added ajax call to recieve comments from controller Added comments section to view

--- EventGroupProject/Controllers/ViewSingleEventController.cs
+++ EventGroupProject/Controllers/ViewSingleEventController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventGroupProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +16,26 @@ namespace EventGroupProject.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            Events newEvent = new Events
+            {
+                EventName = "Test",
+                EventId = 1
+            };
+
+            return View(newEvent);
+        }
+
+        [HttpGet]
+        public JsonResult GetComments(int eventId)
+        {
+            List<Comments> comments = new List<Comments>()
+            {
+                new Comments{CommentId = 1, Content = "Comment 1"},
+                new Comments{CommentId = 2, Content = "Comment 2"},
+                new Comments{CommentId = 3, Content = "Comment 3"}
+            };
+
+            return Json(comments);
         }
 
         
---
+++ EventGroupProject/Models/CommentDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventGroupProject.Models
+{
+    public class CommentDetails
+    {
+        public Comments Comment { get; set; }
+        public int CommentUserId { get; set; }
+        public int CommentEventId { get; set; }
+    }
+}
