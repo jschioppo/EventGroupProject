a4158f7835e96b19267d33ff0cfe9132210455c6
Jacob Schioppo <jacobschioppo@gmail.com>
2018-10-29 02:09:07 -0400

    Made some tweaks to new event search view

--- EventGroupProject/Controllers/EventsRecommendedSearchingController.cs
+++ EventGroupProject/Controllers/EventsRecommendedSearchingController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventGroupProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -9,10 +10,22 @@ namespace EventGroupProject.Controllers
 {
     public class EventsRecommendedSearching : Controller
     {
+        private DBHandler _dbHandler { get; set; }
+
+        public EventsRecommendedSearching(DBHandler dbHandler)
+        {
+            _dbHandler = dbHandler;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult _Search()
+        {
+            return PartialView(_dbHandler.GetAllTags());
         }
     }
 }
